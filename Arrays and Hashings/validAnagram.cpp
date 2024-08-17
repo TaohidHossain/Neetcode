13415ec12c4ac20855c8c87376629f29b81d494e
Taohid Hossain <htaohid31@gmail.com>
2024-08-17 12:38:21 +0600

    add validAnagram.cpp

---
+++ Arrays and Hashings/validAnagram.cpp
@@ -0,0 +1,22 @@
+/*
+Leetcode: 242. Valid Anagram
+Time: O(s)
+Space: O(s)
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        unordered_map<char, int> s_map, t_map;
+        for(int i=0;i<s.size();i++){
+            s_map[s[i]]++;
+            t_map[t[i]]++;
+        }
+        for(int i=0;i< s_map.size();i++){
+            if(s_map[i] != t_map[i])
+                return false;
+        }
+        return true;
+    }
+};
