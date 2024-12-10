a7116cd355baf4f855db015577f5ea5394246b06
TaohidHossain <htaohid31@gmail.com>
2024-12-10 20:23:30 +0600

    add mergeTwoSortedList.cpp

---
+++ Linked List/mergeTwoSortedLists.cpp
@@ -0,0 +1,59 @@
+/*
+    Leetcode: 21. Merge Two Sorted Lists
+    https://leetcode.com/problems/merge-two-sorted-lists/description/
+    Time: O(n)
+    Space: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head, *curr;
+
+        if(!list1 && !list2) return NULL;
+        if(!list1) return list2;
+        if(!list2) return list1;
+
+        if(list1->val < list2->val){
+            head = list1;
+            list1 = list1->next;
+        }
+        else{
+            head = list2;
+            list2 = list2->next;
+        }
+
+        curr = head;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                curr->next = list1;
+                curr = curr->next;
+                list1 = list1->next;
+            }
+            else{
+                curr->next = list2;
+                curr = curr->next;
+                list2 = list2->next;
+            }
+        }
+
+        if(list1){
+            curr->next = list1;
+        }
+        if(list2){
+            curr->next = list2;
+
+        }
+        return head;
+    }
+};
