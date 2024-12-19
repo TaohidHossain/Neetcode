e5167f61c79c673b7938d643ebda1e623d1889e9
TaohidHossain <htaohid31@gmail.com>
2024-12-19 12:02:14 +0600

    fix space complexity

--- Graphs/numberofIslands.cpp
+++ Graphs/numberofIslands.cpp
@@ -1,34 +1,32 @@
 /*
-    Leetcode: 200. Number of Islands
-    https://leetcode.com/problems/number-of-islands/description/
+    695. Max Area of Island
+    https://leetcode.com/problems/max-area-of-island/description/
     Time: O(rows x cols)
-    Space: O(1)
+    Space: O(rows x cols)
 */
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         const int rows = grid.size();
         const int cols = grid[0].size();
-        int islands = 0;
+        int maxArea = 0;
 
         for(int i=0; i<rows; i++){
             for(int j=0; j<cols; j++){
-                if(grid[i][j] == '1'){
-                    islands++;
-                    bfs(grid, i, j, rows, cols);
+                if(grid[i][j] == 1){
+                    maxArea = max(maxArea, dfs(grid, i, j, rows, cols));
                 }
             }
         }
-        return islands;
+        return maxArea;
     }
 private:
-    void bfs(vector<vector<char>>& grid, int i, int j, int row, int col){
-        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        bfs(grid, i - 1, j, row, col);
-        bfs(grid, i + 1, j, row, col);
-        bfs(grid, i, j - 1, row, col);
-        bfs(grid, i, j + 1, row, col);
+    int dfs(vector<vector<int>>& grid, int i, int j, int r, int c){
+        if(i < 0 || i >= r || j < 0 || j >= c || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+
+        return 1 + dfs(grid, i - 1, j, r, c) + dfs(grid, i + 1, j, r, c) +
+        dfs(grid, i, j - 1, r, c) + dfs(grid, i, j + 1, r, c);
     }
 };
